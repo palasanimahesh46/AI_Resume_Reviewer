96cd77413019753c03ff5113f898ecc5a62f7d79
palasanimahesh46 <palasanimahesh46@gmail.com>
2024-01-19 10:53:31 +0530

    Updated code

--- app.py
+++ app.py
@@ -43,86 +43,50 @@ def input_pdf_setup(uploaded_file):
 
 ## Streamlit App
 
-st.set_page_config(page_title="Resume Expert")
-
-st.header("JobFit Analyzer")
-st.subheader('This Application helps you in your Resume Review with help of GEMINI AI [LLM]')
-input_text = st.text_input("Job Description: ", key="input")
-uploaded_file = st.file_uploader("Upload your Resume(PDF)...", type=["pdf"])
-pdf_content = ""
+st.set_page_config(page_title="ATS Resume Expert")
+st.header("ATS Tracking System")
+input_text=st.text_area("Job Description: ",key="input")
+uploaded_file=st.file_uploader("Upload your resume(PDF...",type=["pdf"])
 
 if uploaded_file is not None:
-    st.write("PDF Uploaded Successfully")
+    st.write("PDF Upladed Successfully")
+
 
 submit1 = st.button("Tell Me About the Resume")
 
-submit2 = st.button("How Can I Improvise my Skills")
+#submit2 = st.button("How Can I Improvise my Skills")
 
-submit3 = st.button("What are the Keywords That are Missing")
+#submit3 = st.button("What are the Keywords That are Missing")
 
-submit4 = st.button("Percentage match")
+submit3 = st.button("Percentage Match")
 
 input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+You are an experienced HR With Tech Experience in the filed of any one job role from Data Science,Full stack Web development,Big Data Engineering,DEVOPS,Data Analyst,your task is to review the provided resume against the job description for these profiles.
+Please share your professional evaluation on whether the candidate's profile aligns with it
+Highlight the strengths and weaknesses of the applicant in relation to the specified job role
 """
 
-input_prompt2 = """
-You are an Technical Human Resource Manager with expertise in data science, 
-your role is to scrutinize the resume in light of the job description provided. 
-Share your insights on the candidate's suitability for the role from an HR perspective. 
-Additionally, offer advice on enhancing the candidate's skills and identify areas where improvement is needed.
-"""
+
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. As a Human Resource manager,
- assess the compatibility of the resume with the role. Give me what are the keywords that are missing
- Also, provide recommendations for enhancing the candidate's skills and identify which areas require further development.
-"""
-input_prompt4 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of any one job role  Data Science, Web development,Big Data Engineering,DEVOPS,Data Analyst and deep ATS functionality,
+your task is to evaluate the resume against the provided job description. give me the percentage alignment with the job description. First the output should come as percentage and then keywords missing aligned with
 """
 
 if submit1:
     if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt1, pdf_content, input_text)
-        st.subheader("The Response is")
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("the response is")
         st.write(response)
     else:
-        st.write("Please upload a PDF file to proceed.")
+        st.write("please upload the resume")
 
-elif submit2:
+if submit3:
     if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt2, pdf_content, input_text)
-        st.subheader("The Response is")
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt3,pdf_content,input_text)
+        st.subheader("the response is")
         st.write(response)
     else:
-        st.write("Please upload a PDF file to proceed.")
-
-elif submit3:
-    if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt3, pdf_content, input_text)
-        st.subheader("The Response is")
-        st.write(response)
-    else:
-        st.write("Please upload a PDF file to proceed.")
-
-elif submit4:
-    if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt4, pdf_content, input_text)
-        st.subheader("The Response is")
-        st.write(response)
-    else:
-        st.write("Please upload a PDF file to proceed.")
-
-
-st.markdown("---")
-st.caption("Resume Expert - Making Job Applications Easier")
+        st.write("please upload the resume")
